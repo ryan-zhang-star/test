74601a7ff2617d53d3d76da549f2849b5b8edcd1
heartbeat <heartbeat.webartist@gmail.com>
2019-12-27 15:49:23 +0800

    init

---
+++ app/Http/Controllers/IndexController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class IndexController extends Controller
+{
+    public function main()
+    {
+        return 'Yeeeeee';
+    }
+    public function main123()
+    {
+        return 'Yeeeee123123e';
+    }
+}
