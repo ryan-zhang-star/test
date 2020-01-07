de69e230abd277c50c0b013ad27d64140454ad60
Huang-Mike <f811107@gmail.com>
2020-01-07 15:32:47 +0800

    Decimal Fix

--- app/Http/Controllers/IndexController.php
+++ app/Http/Controllers/IndexController.php
@@ -20,27 +20,30 @@ class IndexController extends Controller
     {
         print_r('Mike Test');
         echo '<br>';
-            if (is_numeric($bbb)&& $bbb>1)
+            if (is_numeric($aaa) && is_numeric($bbb) && $bbb>1)
             {
              echo '將數值 "' . $aaa . '" 轉換成 ' . $bbb . ' 進制';
              echo '<br>';    
                 echo '答案為：' . base_convert($aaa, 10, $bbb);   //將aaa十進制轉換成bbb進制    
             }else{
-                if (preg_match("/^[\x7f-\xff]+$/", $bbb))
+                if (preg_match("/^[\x7f-\xff]+$/", $aaa) || preg_match("/^[\x7f-\xff]+$/", $bbb))
                 {
-                    return '請不要亂打中文好嗎????';
+                    echo '請不要亂打中文好嗎????數學不會嗎?';
                 }
-                if(preg_match("/[a-zA-Z]/",$bbb))
+                if(preg_match("/[a-zA-Z]/",$aaa) || preg_match("/[a-zA-Z]/",$bbb))
                 {
-                    return '英文很好逆????';
+                    echo '<br>';    
+                    echo '英文很好逆????打數字拉!!!!';
                 }
-                if(preg_match("/[\Q~!@#$%^&*()+-_=.:?<>\E]/",$bbb))
+                if(preg_match("/[\Q~!@#$%^&*()+-_=.:?<>\E]/",$aaa) || preg_match("/[\Q~!@#$%^&*()+-_=.:?<>\E]/",$bbb))
                 {
-                    return '愛打符號????';
+                    echo '<br>';    
+                    return '愛打符號????小學沒讀好?';
                 }
-                if ($bbb < 2)
+                if ($bbb < 2 && is_numeric($bbb))
                 {
-                    return '請輸入大於1的數值';
+                    echo '<br>';    
+                    echo '輸入大於1的數字';
                 }        
             }
         }
