a4f7991eb7795cd4df85376c83ea40a44af60452
Huang-Mike <f811107@gmail.com>
2020-01-07 14:14:49 +0800

    Deciaml3

--- app/Http/Controllers/IndexController.php
+++ app/Http/Controllers/IndexController.php
@@ -16,19 +16,33 @@ class IndexController extends Controller
         return 'Yeeeee';
     }
 
-    public function practice(Request $request,$aaa, $bbb)
+    public function practice(Request $request,$aaa, $bbb = 2)
     {
         print_r('Mike Test');
         echo '<br>';
-        $ccc = base_convert($bbb,2,10);
-        if ($ccc<2)
+        if ($bbb < 2)
         {
-            return '您輸入錯誤進制，請重新輸入!';
-        };
-        echo '將數值 "' . $aaa . '" 轉換成 ' . $bbb . ' 進制';
-        echo '<br>';
-        echo '答案為：' . base_convert($aaa, 10, $ccc);   //將aaa十進制轉換成bbb進制
-
+            return '請輸入大於1的數值';
+        }else{
+            if (is_numeric($bbb))
+            {
+             echo '將數值 "' . $aaa . '" 轉換成 ' . $bbb . ' 進制';
+             echo '<br>';    
+                echo '答案為：' . base_convert($aaa, 10, $bbb);   //將aaa十進制轉換成bbb進制    
+            }else{
+                if (preg_match("/^[\x7f-\xff]+$/", $bbb))
+                {
+                    return '請不要亂打中文好嗎????';
+                }
+                if(preg_match("/[a-zA-Z]/",$bbb))
+                {
+                    return '英文很好逆????';
+                }
+                if(preg_match("/[\Q~!@#$%^&*()+-_=.:?<>\E]/",$bbb))
+                {
+                    return '愛打符號????';
+                }
+            }
+        }
     }
-
 }
